42bb9aa423b52406385a894adf5f77f84ff260be
gustavo <gustavo@tseller.com.br>
2022-08-12 22:24:36 -0300

    custom tsserver to remove redundant tsreact defs

---
+++ tsserver.lua
@@ -0,0 +1,89 @@
+local util = require 'lspconfig.util'
+
+local bin_name = 'typescript-language-server'
+local cmd = { bin_name, '--stdio' }
+
+if vim.fn.has 'win32' == 1 then
+  cmd = { 'cmd.exe', '/C', bin_name, '--stdio' }
+end
+
+local function filter(arr, fn)
+  if type(arr) ~= "table" then
+    return arr
+  end
+
+  local filtered = {}
+  for k, v in pairs(arr) do
+    if fn(v, k, arr) then
+      table.insert(filtered, v)
+    end
+  end
+
+  return filtered
+end
+
+local function filterReactDTS(value)
+  return string.match(value.uri, '%.d.ts') == nil
+end
+
+return {
+  default_config = {
+    init_options = { hostInfo = 'neovim' },
+    cmd = cmd,
+    filetypes = {
+      'javascript',
+      'javascriptreact',
+      'javascript.jsx',
+      'typescript',
+      'typescriptreact',
+      'typescript.tsx',
+    },
+    handlers = {
+      ['textDocument/definition'] = function(err, result, method, ...)
+        if vim.tbl_islist(result) and #result > 1 then
+          local filtered_result = filter(result, filterReactDTS)
+          return vim.lsp.handlers['textDocument/definition'](err, filtered_result, method, ...)
+        end
+
+        vim.lsp.handlers['textDocument/definition'](err, result, method, ...)
+      end
+    },
+    root_dir = function(fname)
+      return util.root_pattern 'tsconfig.json' (fname)
+          or util.root_pattern('package.json', 'jsconfig.json', '.git')(fname)
+    end,
+  },
+  docs = {
+    description = [[
+https://github.com/theia-ide/typescript-language-server
+
+`typescript-language-server` depends on `typescript`. Both packages can be installed via `npm`:
+```sh
+npm install -g typescript typescript-language-server
+```
+
+To configure type language server, add a
+[`tsconfig.json`](https://www.typescriptlang.org/docs/handbook/tsconfig-json.html) or
+[`jsconfig.json`](https://code.visualstudio.com/docs/languages/jsconfig) to the root of your
+project.
+
+Here's an example that disables type checking in JavaScript files.
+
+```json
+{
+  "compilerOptions": {
+    "module": "commonjs",
+    "target": "es6",
+    "checkJs": false
+  },
+  "exclude": [
+    "node_modules"
+  ]
+}
+```
+]]   ,
+    default_config = {
+      root_dir = [[root_pattern("package.json", "tsconfig.json", "jsconfig.json", ".git")]],
+    },
+  },
+}
