543c1a5c2b7d15b6fe39863f94836cc4dbb405ed
gustavo <gustavo@tseller.com.br>
2023-09-25 18:20:58 -0300

    fixed vim ts autotag slash thing

--- nvim/after/plugin/treesitter.rc.lua
+++ nvim/after/plugin/treesitter.rc.lua
@@ -1,56 +1,57 @@
 local status, ts = pcall(require, "nvim-treesitter.configs")
-if (not status) then return end
+if not status then
+	return
+end
 
-ts.setup {
-  highlight = {
-    enable = true,
-    disable = {},
-
-  },
-  folding = {
-    enable = true
-  },
-  indent = {
-    enable = true,
-    disable = {},
-  },
-  context_commentstring = {
-    enable = true
-  },
-  ensure_installed = {
-    "tsx",
-    "toml",
-    "fish",
-    "php",
-    "json",
-    "graphql",
-    "markdown",
-    "markdown_inline",
-    "yaml",
-    "swift",
-    "css",
-    "html",
-    "lua",
-    "typescript",
-    "javascript",
-    "prisma"
-  },
-  autotag = {
-    enable = true,
-  },
-  rainbow = {
-    enable = true,
-    extended_mode = true, -- Highlight also non-parentheses delimiters, boolean or table: lang -> boolean
-    max_file_lines = 1000, -- Do not enable for files with more than 1000 lines, int
-  },
-
-
-}
-require('ufo').setup({
-    provider_selector = function(bufnr, filetype, buftype)
-        return {'treesitter', 'indent'}
-    end
+ts.setup({
+	highlight = {
+		enable = true,
+		disable = {},
+	},
+	folding = {
+		enable = true,
+	},
+	indent = {
+		enable = true,
+		disable = {},
+	},
+	context_commentstring = {
+		enable = true,
+	},
+	ensure_installed = {
+		"tsx",
+		"toml",
+		"fish",
+		"php",
+		"json",
+		"graphql",
+		"markdown",
+		"markdown_inline",
+		"yaml",
+		"swift",
+		"css",
+		"html",
+		"lua",
+		"typescript",
+		"javascript",
+		"prisma",
+	},
+	autotag = {
+		enable = true,
+		enable_close_on_slash = false,
+	},
+	rainbow = {
+		enable = true,
+		extended_mode = true, -- Highlight also non-parentheses delimiters, boolean or table: lang -> boolean
+		max_file_lines = 1000, -- Do not enable for files with more than 1000 lines, int
+	},
+})
+require("ufo").setup({
+	provider_selector = function(bufnr, filetype, buftype)
+		return { "treesitter", "indent" }
+	end,
 })
 
-local parser_config = require "nvim-treesitter.parsers".get_parser_configs()
-parser_config.tsx.filetype_to_parsername = { "javascript", "typescript.tsx", "typescript", "javascript.jsx", "typescriptreact", "graphql" }
+local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
+parser_config.tsx.filetype_to_parsername =
+	{ "javascript", "typescript.tsx", "typescript", "javascript.jsx", "typescriptreact", "graphql" }
